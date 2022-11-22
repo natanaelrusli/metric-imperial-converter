ff75cee5169105847ece4715d3482639f1b7c70e
Nata Nael <nata.nael@shopee.com>
2022-11-22 17:06:31 +0700

    add: tests suite

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -15,19 +15,19 @@ suite('Unit Tests', function(){
 
   suite ('Function convertHandler.getNum(input)', function() {
 
-    test('should correctly read a whole number input', function(done) {
+    test('Should correctly read a whole number input', function(done) {
       let input = '32L';
       assert.equal(convertHandler.getNum(input), 32);
       done();
     });
 
-    test('should correctly read a decimal number input', function(done) {
+    test('Should correctly read a decimal number input', function(done) {
       let input = '3.2l';
       assert.equal(convertHandler.getNum(input), 3.2);
       done();
     });
 
-    test('should correctly read a fractional input', function(done) {
+    test('Should correctly read a fractional input', function(done) {
       let input = '3/2l';
       assert.equal(convertHandler.getNum(input), 1.5);
       done();
@@ -39,78 +39,78 @@ suite('Unit Tests', function(){
       done();
     });
 
-    test('should correctly return an error on a double-fraction (i.e. 3/2/3)', function(done) {
+    test('Should correctly return an error on a double-fraction (i.e. 3/2/3)', function(done) {
       let input = '10.0.0l';
       assert.equal(convertHandler.getNum(input), undefined);
       done();
     });
 
-    test('should correctly default to a numerical input of 1 when no numerical input is provided', function(done) {
+    test('Should correctly default to a numerical input of 1 when no numerical input is provided', function(done) {
       let input = 'mi';
       assert.equal(convertHandler.getNum(input), 1)
       done();
     });
 
-    test('should correctly read each valid input unit', function(done) {
+    test('Should correctly read each valid input unit', function(done) {
       let input = '20mi';
       assert.equal(convertHandler.getUnit(input), 'mi')
       done();
     });
 
-    test('should correctly return an error for an invalid input unit', function(done) {
+    test('Should correctly return an error for an invalid input unit', function(done) {
       let input = '20mis';
       assert.equal(convertHandler.getUnit(input), undefined)
       done();
     });
 
-    test('should return the correct return unit for each valid input unit', function(done) {
+    test('Should return the correct return unit for each valid input unit', function(done) {
       let input = 'mi';
       assert.equal(convertHandler.getReturnUnit(input), 'km');
       done();
     });
 
-    test('should correctly return the spelled-out string unit for each valid input unit', function(done) {
+    test('Should correctly return the spelled-out string unit for each valid input unit', function(done) {
       let input = 'mi';
       assert.equal(convertHandler.spellOutUnit(input), 'miles');
       done();
     });
 
-    test('should correctly convert gal to L', function(done) {
+    test('Should correctly convert gal to L', function(done) {
       let input = '20mi';
 
       assert.equal(mockConvert(input), 32.18680);
       done();
     });
 
-    test('should correctly convert L to gal', function(done) {
+    test('Should correctly convert L to gal', function(done) {
       let input = '20l';
 
       assert.equal(mockConvert(input), 5.28344);
       done();
     });
 
-    test('should correctly convert mi to km', function(done) {
+    test('Should correctly convert mi to km', function(done) {
       let input = '20mi';
 
       assert.equal(mockConvert(input), 32.18680);
       done();
     });
 
-    test('should correctly convert km to mi', function(done) {
+    test('Should correctly convert km to mi', function(done) {
       let input = '20km';
 
       assert.equal(mockConvert(input), 12.42745);
       done();
     });
 
-    test('should correctly convert lbs to kg', function(done) {
+    test('Should correctly convert lbs to kg', function(done) {
       let input = '20lbs';
 
       assert.equal(mockConvert(input), 9.07184);
       done();
     });
 
-    test('should correctly convert kg to lbs', function(done) {
+    test('Should correctly convert kg to lbs', function(done) {
       let input = '20kg';
 
       assert.equal(mockConvert(input), 44.09249);
